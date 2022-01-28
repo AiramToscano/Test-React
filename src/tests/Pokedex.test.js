d6ab664b4f38d0e6900aa98ff56aea48c50b448f
Airam Toscano <airamlobato@gmail.com>
2022-01-28 16:13:58 -0400

    testando o avaliador

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,9 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../helpers/renderWithRouter';
+import { About } from '../components';
+// visto a maioria da logica na monitoria do rod 28/01
+describe('testa o component About', () => {
+  beforeEach(() => {
+    renderWithRouter(<About />);
+  });
+});
